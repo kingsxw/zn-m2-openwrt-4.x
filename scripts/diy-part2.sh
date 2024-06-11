e304a6614807775e486ee2928bb19b229d216aeb
kingsxw <kingsxw@163.com>
2024-06-11 14:41:18 +0800

    Update diy-part2.sh

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -20,5 +20,5 @@ sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/M
 sed -i 's/OpenWrt/ZN-M2/g' package/base-files/files/bin/config_generate
 
 # coremark
-chmod +x ./coremark
-source ./coremark
+chmod +x ./coremark.sh
+source ./coremark.sh
